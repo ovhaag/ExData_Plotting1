d998aa600b44f1e4b139e9ea0f67d5ac42a7f39b
Oliver Haag <oliver@haagnetz.de>
2014-05-11 00:09:22 +0100

    first version of code
    code tryouts + adaptions
    creates first version of plots

---
+++ hpc.R
@@ -0,0 +1,66 @@
+# read full data
+# http://stat.ethz.ch/R-manual/R-devel/library/utils/html/read.table.html -- read.. + options
+# http://www.cookbook-r.com/Data_input_and_output/Loading_data_from_a_file/ -- strings as factors or characters!!
+hpc_full <- read.csv('household_power_consumption.txt', sep=';',na.strings = "?")
+
+# optional 
+# check first data, NA Data, structure
+head(hpc_full, 5)
+hpc_full[6840, ]
+
+# convert dates -- trial
+# http://rfunction.com/archives/1912 -- as.Date, strptime for dates or dates with times
+# d123 <- as.Date(x123$Date, format="%d/%m/%Y") # ok
+# t123 <- strptime(x123$Time, format="%H:%M:%S") # adds current date - unwanted
+# t123 <- strptime(x123$Time, format="%T") # adds current date - unwanted
+# weekdays(d123) # ok - may help later
+# dtc123 <- paste(as.character(x123$Date), as.character(x123$Time)) # ok, first conversion to string
+# dt123 <- as.POSIXlt(dtc123, format="%d/%m/%Y %T") # ok then final conversion to POSIXlt DateTime
+# dt123[1] < dt123[3] # ok theese dates can be compared
+# weekdays(dt123) # ok - and converted to weekdays -- ups language is german
+
+# convert dates .. -- do it now
+hpc_full$dtc <- paste(as.character(hpc_full$Date), as.character(hpc_full$Time))
+hpc_full$dtp <- as.POSIXlt(hpc_full$dtc, format="%d/%m/%Y %T")
+hpc_full$wd <- weekdays(hpc_full$dtp)
+
+# select subset
+hpc <- hpc_full[which(hpc_full$dtp >= as.POSIXlt("2007-02-01 00:00:00") & hpc_full$dtp < as.POSIXlt("2007-02-03 00:00:00")), ]
+
+# check subset
+head(hpc, 3)
+tail(hpc, 3)
+
+# first tests for plotting
+hist(Global_active_power)
+hist(Global_active_power, xlab="Global Active Power (kilowatts)")
+hist(Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power")
+
+# plot(dtp, Global_active_power)
+plot(dtp, Global_active_power, type="l")
+plot(dtp, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)") # for the first ok / days shold be english
+
+# set locale
+Sys.setlocale(category = "LC_TIME", locale = "C") # set locale 
+# Sys.setlocale(category = "LC_ALL", locale = "") # reset locale -- do this later
+
+plot(dtp, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)") # now the plot is ok
+
+plot(dtp, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(dtp, Sub_metering_2, col="red")
+lines(dtp, Sub_metering_3, col="blue")
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+plot(dtp, Voltage, type="l", xlab="datetime") # subplot
+plot(dtp, Global_reactive_power, type="l", xlab="datetime") # subplot
+
+par(mfrow = c(2,2))
+plot(dtp, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)") # subplot 1
+plot(dtp, Voltage, type="l", xlab="datetime") # subplot 2
+plot(dtp, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering") # start subplot 3
+lines(dtp, Sub_metering_2, col="red")
+lines(dtp, Sub_metering_3, col="blue")
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(dtp, Global_reactive_power, type="l", xlab="datetime") # subplot 4
+
+
